5e5e54fca675a7777856840afc3d69a00e631fa1
alchemistmatt <matthew.monroe@pnnl.gov>
2020-03-05 16:14:12 -0800

    Tabs to spaces

--- Sipper/ViewModel/BooleanToVisibleOrHidden.cs
+++ Sipper/ViewModel/BooleanToVisibleOrHidden.cs
@@ -1,55 +1,46 @@
 ﻿using System;
-using System.Windows.Data;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Data;
 
 namespace Sipper.ViewModel
 {
-	/// <summary>
-	/// Converter class to convert from True/False to Visible/Hidden
-	/// Set "Collapse" to True to instead convert to Visible/Collapsed
-	/// </summary>
-	/// <remarks>From "http://www.rhyous.com/2011/02/22/binding-visibility-to-a-bool-value-in-wpf/"</remarks>
-	public class BoolToVisibleOrHidden : IValueConverter
-	{
-		#region Constructors
-		/// <summary>
-		/// The default constructor
-		/// </summary>
-		public BoolToVisibleOrHidden() { }
-		#endregion
+    /// <summary>
+    /// Converter class to convert from True/False to Visible/Hidden
+    /// Set "Collapse" to True to instead convert to Visible/Collapsed
+    /// </summary>
+    /// <remarks>From "http://www.rhyous.com/2011/02/22/binding-visibility-to-a-bool-value-in-wpf/"</remarks>
+    public class BoolToVisibleOrHidden : IValueConverter
+    {
 
-		#region Properties
-		public bool Collapse { get; set; }
-		public bool Reverse { get; set; }
-		#endregion
+        #region Properties
 
-		#region IValueConverter Members
-		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			var bValue = (bool)value;
+        public bool Collapse { get; set; }
+        public bool Reverse { get; set; }
 
-			if (bValue != Reverse)
-			{
-				return Visibility.Visible;
-			}
-			else
-			{
-				if (Collapse)
-					return Visibility.Collapsed;
-				else
-					return Visibility.Hidden;
-			}
-		}
+        #endregion
 
-		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			var visibility = (Visibility)value;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bValue = (bool)value;
 
-			if (visibility == Visibility.Visible)
-				return !Reverse;
-			else
-				return Reverse;
-		}
-		#endregion
-	}
+            if (bValue != Reverse)
+            {
+                return Visibility.Visible;
+            }
+
+            if (Collapse)
+                return Visibility.Collapsed;
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var visibility = (Visibility)value;
+
+            if (visibility == Visibility.Visible)
+                return !Reverse;
+            return Reverse;
+        }
+    }
 }
